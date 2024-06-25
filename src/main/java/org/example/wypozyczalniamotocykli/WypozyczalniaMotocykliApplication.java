623cdc8821fcde712fb178b71206b54d4009d011
Marcin-Grzes <marcin@grzes.dev>
2024-06-25 17:55:14 +0200

    Initial

---
+++ src/main/java/org/example/wypozyczalniamotocykli/WypozyczalniaMotocykliApplication.java
@@ -0,0 +1,13 @@
+package org.example.wypozyczalniamotocykli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WypozyczalniaMotocykliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WypozyczalniaMotocykliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/wypozyczalniamotocykli/WypozyczalniaMotocykliApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wypozyczalniamotocykli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WypozyczalniaMotocykliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
