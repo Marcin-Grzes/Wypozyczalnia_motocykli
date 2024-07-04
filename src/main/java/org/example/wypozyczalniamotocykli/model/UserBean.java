de31fa6ddff7100cc4a4c65accf9f5735156ef2a
Marcin-Grzes <marcin@grzes.dev>
2024-07-04 13:03:22 +0200

    Reconfigure project3

--- src/main/java/org/example/wypozyczalniamotocykli/model/UserBean.java
+++ src/main/java/org/example/wypozyczalniamotocykli/model/UserBean.java
@@ -1,12 +1,12 @@
-package org.example.wypozyczalniamotocykli.model;
-
-import lombok.Data;
-
-
-@Data
-public class UserBean {
-    private String name;
-    public String submit() {
-        return "welcome"; // Nawigacja do welcome.xhtml
-    }
-}
+//package org.example.wypozyczalniamotocykli.model;
+//
+//import lombok.Data;
+//
+//
+//@Data
+//public class UserBean {
+//    private String name;
+//    public String submit() {
+//        return "welcome"; // Nawigacja do welcome.xhtml
+//    }
+//}
