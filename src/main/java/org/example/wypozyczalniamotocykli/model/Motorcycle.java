0f779527d3a5124da782829c1ee3d7dd359d66a0
Marcin-Grzes <marcin@grzes.dev>
2024-07-12 14:22:48 +0200

    layout_test_done, klasa_Motorcycle

---
+++ src/main/java/org/example/wypozyczalniamotocykli/model/Motorcycle.java
@@ -0,0 +1,26 @@
+package org.example.wypozyczalniamotocykli.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+public class Motorcycle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String marka;
+    private String model;
+    private String kolor;
+    private int pojemnosc;
+    private int moc;
+    private int rocznik;
+    private BigDecimal cena;
+    private String imageLink;
+
+}
